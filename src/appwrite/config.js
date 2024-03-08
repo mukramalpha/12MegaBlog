2c3255c5d99df29aa49efdb47d0b3aaf1bec9867
mukramalpha <mukram.alphatech@gmail.com>
2024-03-08 12:27:28 +0530

    "appwrite databases"

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -98,6 +98,44 @@ export class Service{
         }
     }
 
+    //file upload service
+    async uploadFile(file){
+        try{
+            return await this.bucket.createFile(
+                conf.appwriteBucketId,
+                ID.unique(),
+                file
+            ) 
+
+        }catch(error){
+            console.log("appwrite service :: uploadFile :: error ",error);
+            return false; 
+        }
+    }
+
+    //file delete service
+    async deleteFile(fileId){
+        try{
+            await this.bucket.deleteFile(
+                conf.appwriteBucketId,
+                fileId
+            )
+            return true
+
+        }catch(error){
+            console.log("appwrite service :: deleteFile :: error ",error);
+            return false; 
+        }
+    }
+
+    async getFilePreview(fileId){
+        return this.bucket.getFilePreview(
+            conf.appwriteBucketId,
+            fileId
+        )
+
+    }
+
 }
 const service=new Service();
 export default Service;

32c50ac66da56e39891c24722ff50194c24419f6
mukramalpha <mukram.alphatech@gmail.com>
2024-03-11 11:27:08 +0530

    "Post Card added"

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -17,10 +17,14 @@ function Select({
                 ref={ref} 
                 id={id}
                 className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}>
-
+                {options?.map((option)=>(
+                  <option key={option} value={option}>
+                    {option}
+                  </option>
+                ))}
         </select>
     </div>
   )
 }
 
-export default Select
+export default React.forwardRef(Select)
